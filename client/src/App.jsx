39e587a45f7cc6567d5246cbc7f0c75489b98fc6
Karthik <maxwhitie1234@gmail.com>
2025-09-22 21:13:20 +0530

    This Is How We have to use Route to display other pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import { Route, Routes } from 'react-router-dom'
+import ProfilePage from './pages/ProfilePage'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/profile' element={<ProfilePage/>}/>
+      </Routes>
     </div>
   )
 }
